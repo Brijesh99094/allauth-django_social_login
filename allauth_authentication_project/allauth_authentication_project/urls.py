aa4bd41a5369a28be416143ce9d881c893ef1d2f
Brijesh99094 <prajapatibrijesh655@gmail.com>
2022-08-27 17:32:28 +0530

    Added google authentication in the project

--- allauth_authentication_project/allauth_authentication_project/urls.py
+++ allauth_authentication_project/allauth_authentication_project/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',TemplateView.as_view(template_name='dashboard/home.html'),name="home"),
+    path('home/', TemplateView.as_view(template_name='dashboard/home.html'), name="home"),
     path('accounts/', include('allauth.urls')),
 ]
